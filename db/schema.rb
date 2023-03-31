f5dec780bb815708fbe177a9bb7a8b6128eaae84
Nico Rithner <nicorithner@gmail.com>
2023-03-30 21:40:48 -0600

    Add foreign key to tasks and update relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_031737) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_033642) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_031737) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
