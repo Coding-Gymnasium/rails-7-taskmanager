6ea38583634e76f17def024ea559fa8e1a347b53
Nico Rithner <nicorithner@gmail.com>
2023-04-02 06:25:35 -0600

    Add Create and Destroy functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,15 +9,37 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  def new; end
+  def new
+    count = Task.count
+    @task = Task.new(position: count + 1)
+  end
 
-  def create; end
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render('new')
+    end
+  end
 
   def edit; end
 
   def update; end
 
-  def delete; end
+  def delete
+    @task = Task.find(params[:task_id])
+  end
 
-  def destroy; end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:name, :position, :completed, :description)
+  end
 end
